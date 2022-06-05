8b64d78e1c911feb4b9840d9b6a60ed75658b612
Damian Jordanov <d.jordanov@posteo.net>
2022-06-05 14:32:34 +0200

    finished cli executable jar

--- src/main/java/Hl72json.java
+++ src/main/java/Hl72json.java
@@ -10,16 +10,22 @@ import org.json.XML;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Command(name = "hl72json", version = "HL72JJson 1.0", mixinStandardHelpOptions = true)
 public class Hl72json implements Runnable {
 
-    @Option(names = { "--xml" }, description = "Output as xml")
-    boolean xml = true;
+    @Option(names = { "--xml" }, description = "Output as xml") boolean xml;
 
-    @Option(names = { "--json" }, description = "Output as json")
-    boolean json;
+    @Option(names = { "--json" }, description = "Output as json") boolean json;
+
+    @Option(names = { "--replace-linebreaks" }, description = "Replace linefeeds/ linebreaks (\\r\\n or \\n) with \\r. Turning this off may break hl7 decoding")
+    boolean replaceLinebreaks = true;
+
+    @Option(names = { "--file" }, description = "HL7 file to be converted") String filepath = null;
 
     @Override
     public void run() {
@@ -27,17 +33,22 @@ public class Hl72json implements Runnable {
             throw new RuntimeException("Illegal arguments: Cannot convert to both xml and json");
         }
         if (!xml && !json) {
-            throw new RuntimeException("Illegal arguments: Neither xml not json given as output format");
+            xml = true; // take xml as default output format
         }
 
-        String msg = "MSH|^~\\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.2\r"
-                + "PID|0001|00009874|00001122|A00977|SMITH^JOHN^M|MOM|19581119|F|NOTREAL^LINDA^M|C|564 SPRING ST^^NEEDHAM^MA^02494^US|0002|(818)565-1551|(425)828-3344|E|S|C|0000444444|252-00-4414||||SA|||SA||||NONE|V1|0001|I|D.ER^50A^M110^01|ER|P00055|11B^M011^02|070615^BATMAN^GEORGE^L|555888^NOTREAL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^NOTREAL^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|199904101200||||5555112333|||666097^NOTREAL^MANNY^P\r"
-                + "NK1|0222555|NOTREAL^JAMES^R|FA|STREET^OTHER STREET^CITY^ST^55566|(222)111-3333|(888)999-0000|||||||ORGANIZATION\r"
-                + "PV1|0001|I|D.ER^1F^M950^01|ER|P000998|11B^M011^02|070615^BATMAN^GEORGE^L|555888^OKNEL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^VOICE^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|||||5555112333|||666097^DNOTREAL^MANNY^P\r"
-                + "PV2|||0112^TESTING|55555^PATIENT IS NORMAL|NONE|||19990225|19990226|1|1|TESTING|555888^NOTREAL^BOB^K^DR^MD||||||||||PROD^003^099|02|ER||NONE|19990225|19990223|19990316|NONE\r"
-                + "AL1||SEV|001^POLLEN\r"
-                + "GT1||0222PL|NOTREAL^BOB^B||STREET^OTHER STREET^CITY^ST^77787|(444)999-3333|(222)777-5555||||MO|111-33-5555||||NOTREAL GILL N|STREET^OTHER STREET^CITY^ST^99999|(111)222-3333\r"
-                + "IN1||022254P|4558PD|BLUE CROSS|STREET^OTHER STREET^CITY^ST^00990||(333)333-6666||221K|LENIX|||19980515|19990515|||PATIENT01 TEST D||||||||||||||||||02LL|022LP554";
+        String msg = null;
+        if (filepath != null) {
+            try {
+                 msg = Files.readString(Path.of(filepath));
+            } catch (IOException e) {
+                throw new RuntimeException("Error reading string from file " + filepath);
+            }
+        }
+
+        if (replaceLinebreaks) {
+            // replace possible wrong line feeds/ linebreaks, hl7 is picky with them
+            msg = msg.replace("\r\n", "\r").replace('\n', '\r');
+        }
 
         HapiContext context = new DefaultHapiContext();
         Parser p = context.getGenericParser();
@@ -57,6 +68,23 @@ public class Hl72json implements Runnable {
             System.out.println(XML.toJSONObject(xmlMessage).toString(4));
         }
     }
+
+    /*
+          System.out.println("----- ExampleMain started -------");
+      try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+          String line = null;
+          while (true) {
+              if ((line = reader.readLine()) != null) {
+                  System.out.println("echo>> " + line);
+              } else {
+                  //input finishes
+                  break;
+              }
+          }
+      } catch (Exception e) {
+          System.err.println(e);
+      }
+     */
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Hl72json()).execute(args);
